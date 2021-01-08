d36c68d81f8f13e680b84ae23d2ac626159846ec
alonebreecher <76893049+alonebreecher@users.noreply.github.com>
2021-01-08 05:51:38 +0000

    Update deface.sh

--- deface.sh
+++ deface.sh
@@ -7,33 +7,34 @@ echo ''
 echo ''
 read -p $'\e[32mWeb Target :\e[0m ' target
 read -p $'\e[32Create a Username for the script :\e[0m ' user
-read -p $'\e[32mpesan   :\e[0m ' message1
+read -p $'\e[32mpesan  :\e[0m ' message1
 cd $HOME
 rm index.html
 cat <<LOGIN>index.html
 <html>
 <head>
-<title>K3NZ0</title>
+<title>AMALROCKZZ OOMBIYA KATHA</title>
 <font class="hk2" style="text-shadow: 1px -1px 8px;" face="tahoma">
 <font color="grey">
 <font size="5">
-	<img srs"K3NZ0.jpg">
+	<img srs"FUCKjpg">
 <script>
-alert("HACKED BY K3NZ0");
+alert("AMALROCKZZ VALI VITTAPPOL");
 </script>
 </head>
 <body bgcolor=black>
-<script type="text/javascript" src="http://htmlfreecodes.com/codes/rain.js"></script><br><div class="error">
-<font color="red" size="7" face="Chicle">SITE TAKED BY KERALA TERMUX WARRIORS
+<script type="text/javascript" sr></script><br><div class="error">
+<center><font color="white" size="10" face="Chicle">AMALROCKZZ OOMBI
 <center>
+  <a href="https://ibb.co/kxSbWFM"><img width="195" src="https://i.ibb.co/ZVM4kCN/IMG-20210108-WA0319.jpg" alt="IMG-20210108-" border="0"></a>
 </font><br>
-<img style="width:50%" src="https://avatars0.githubusercontent.com/u/56509491?s=400&v=4"></div>
-<font color=green>
+<img style="width:50%" </div>
+<font color="white">
 <br>
-<b><font face="courier new" size='6'>MUTTAN NILKANDA <br><br>team KTW
+<b><center><font face="courier new" size='6'>KOODUTHAL OOMBATHA ENICH PO MYREA<br><br>NEE ORU MYRAN THANNEA
 <marquee behavior="scroll" direction="left" scrollamount="90" scrolldelay="40" width="100%">
 <font color="green">___________________________________________________________</font></marquee>
-<div style="text-shadow: 0px 0px 10px green;"><span style="color: white;"><font face="transformers"><b>TEAMS : <marquee scrollamount="5" direction="left" width="50%"><span style="color: red;"> <span style="color: red;">KENZ0 - KICHU<span style="color: red;"></b></marquee></font></div><script type="text/rocketscript">/*<![CDATA[*/new TypingText(document.getElementById("message"), 90, function(i){ var ar= new Array("_", " ", "_", " "); return "" +ar[i.length % ar.length]; });//Type out examples:TypingText.runAll();/*]]>*/</script>
+<div style="text-shadow: 0px 0px 10px green;"><span style="color: white;"><font face="transformers"><b>ALERT:MAMANODU ONNUM THONNALLEAA <marquee scrollamount="5" direction="left" width="50%"><span style="color: red;"> <span style="color: red;">AMAL KUNDAN<span style="color: red;"></b></marquee></font></div><script type="text/rocketscript">/*<![CDATA[*/new TypingText(document.getElementById("message"), 90, function(i){ var ar= new Array("_", " ", "_", " "); return "" +ar[i.length % ar.length]; });//Type out examples:TypingText.runAll();/*]]>*/</script>
 <marquee behavior="scroll" direction="right" scrollamount="90" scrolldelay="40" width="100%">
 <font color="green">___________________________________________________________</font></marquee>
 </body>
